55380a80136dc3f89ce8b9aa4abb99221284f0a1
Siim Aus <s.aus@tickmill.com>
2022-11-14 16:19:03 +0000

    Changes

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -2,148 +2,134 @@ package watcher
 
 import (
 	"context"
-	"crypto/tls"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/bep/debounce"
 	"github.com/rs/zerolog/log"
 	networking "k8s.io/api/networking/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/retry"
 )
-
-// A Payload is a collection of Kubernetes data loaded by the watcher.
-type Payload struct {
-	Ingresses       []IngressPayload
-	TLSCertificates map[string]*tls.Certificate
-}
-
-// An IngressPayload is an ingress + its service ports.
-type IngressPayload struct {
-	Ingress      *networking.Ingress
-	ServicePorts map[string]map[string]int
-}
 
 // A Watcher watches for ingresses in the kubernetes cluster
 type Watcher struct {
-	client   kubernetes.Interface
-	onChange func(*Payload)
+	client    kubernetes.Interface
+	context   context.Context
+	labels    labels.Selector
+	namespace string
+	name      string
+	//onChange  func(*Payload)
+	onChange func([]*networking.Ingress)
 }
 
 // New creates a new Watcher.
-func New(client kubernetes.Interface, onChange func(*Payload)) *Watcher {
+func New(client kubernetes.Interface, context context.Context, labels labels.Selector, namespace string, name string, onChange func([]*networking.Ingress)) *Watcher {
 	return &Watcher{
-		client:   client,
-		onChange: onChange,
+		client:    client,
+		context:   context,
+		labels:    labels,
+		namespace: namespace,
+		name:      name,
+		onChange:  onChange,
 	}
 }
 
 // Run runs the watcher.
 func (w *Watcher) Run(ctx context.Context) error {
 	factory := informers.NewSharedInformerFactory(w.client, time.Minute)
-	secretLister := factory.Core().V1().Secrets().Lister()
+
 	serviceLister := factory.Core().V1().Services().Lister()
 	ingressLister := factory.Networking().V1().Ingresses().Lister()
 
-	/*
-		listServices := func(ingress *networking.Ingress) {
-			svc, err := serviceLister.Services(ingress.Namespace).List(labels.Everything())
-			if err != nil {
-				log.Error().Err(err).
-					Str("namespace", ingress.Namespace).
-					Msg("WTF")
-			}
-			for _, service := range svc {
-				log.Debug().
-					Str("namespace", ingress.Namespace).
-					Str("service", service.Name).
-					Msg("Found")
-			}
-		}
-	*/
-	addBackend := func(ingressPayload *IngressPayload, backend networking.IngressBackend) {
-		svc, err := serviceLister.Services(ingressPayload.Ingress.Namespace).Get(backend.Service.Name)
-		if err != nil {
-			log.Error().Err(err).
-				Str("namespace", ingressPayload.Ingress.Namespace).
-				Str("name", backend.Service.Name).
-				Msg("unknown service")
-		} else {
-			m := make(map[string]int)
-			for _, port := range svc.Spec.Ports {
-				m[port.Name] = int(port.Port)
-			}
-			ingressPayload.ServicePorts[svc.Name] = m
-		}
-	}
-
 	onChange := func() {
-		payload := &Payload{
-			TLSCertificates: make(map[string]*tls.Certificate),
+
+		var ingresses []*networking.Ingress
+		var err error
+		if w.namespace != "" {
+			ingresses, err = ingressLister.Ingresses(w.namespace).List(w.labels)
+		} else {
+			ingresses, err = ingressLister.List(w.labels)
 		}
 
-		l, err := labels.Parse("AWSIngressHelper in (myhelper)")
-		if err != nil {
-			l = labels.Nothing()
-		}
-		ingresses, err := ingressLister.List(l)
-		//ingresses, err := ingressLister.List(labels.Everything())
 		if err != nil {
 			log.Error().Err(err).Msg("failed to list ingresses")
 			return
 		}
 
 		for _, ingress := range ingresses {
-			//listServices(ingress)
-			ingressPayload := IngressPayload{
-				Ingress:      ingress,
-				ServicePorts: make(map[string]map[string]int),
-			}
-			payload.Ingresses = append(payload.Ingresses, ingressPayload)
 
-			if ingress.Spec.DefaultBackend != nil {
-				addBackend(&ingressPayload, *ingress.Spec.DefaultBackend)
-			}
-			for _, rule := range ingress.Spec.Rules {
-				if rule.HTTP != nil {
-					continue
-				}
-				for _, path := range rule.HTTP.Paths {
-					addBackend(&ingressPayload, path.Backend)
-				}
-			}
+			if w.name != "" && w.name == ingress.Name || w.name == "" {
 
-			for _, rec := range ingress.Spec.TLS {
-				if rec.SecretName != "" {
-					secret, err := secretLister.Secrets(ingress.Namespace).Get(rec.SecretName)
-					if err != nil {
-						log.Error().
-							Err(err).
-							Str("namespace", ingress.Namespace).
-							Str("name", rec.SecretName).
-							Msg("unknown secret")
-						continue
+				hostnames := make([]string, len(ingress.Status.LoadBalancer.Ingress))
+				for ix, ig := range ingress.Status.LoadBalancer.Ingress {
+					if ig.Hostname != "" {
+						hostnames[ix] = ig.Hostname
+					} else if ig.IP != "" {
+						hostnames[ix] = ig.IP
 					}
-
-					cert, err := tls.X509KeyPair(secret.Data["tls.crt"], secret.Data["tls.key"])
-					if err != nil {
-						log.Error().
-							Err(err).
-							Str("namespace", ingress.Namespace).
-							Str("name", rec.SecretName).
-							Msg("invalid tls certificate")
-						continue
-					}
-
-					payload.TLSCertificates[rec.SecretName] = &cert
 				}
+
+				dns := strings.Join(hostnames, ",")
+
+				services, errors := serviceLister.Services(ingress.Namespace).List(labels.Everything())
+
+				if errors != nil {
+					log.Error().Err(errors).
+						Str("namespace", ingress.Namespace).
+						Msg("Unable to list services")
+				} else {
+					for _, service := range services {
+						if val, ok := service.Annotations["tickmill.com/nginx.frontrunner"]; ok &&
+							service.Spec.Type == "ExternalName" &&
+							val == ingress.Name {
+							log.Debug().
+								Str("service", service.Name).
+								Str("type", string(service.Spec.Type)).
+								Str("annotation", val).
+								Msg("matches")
+
+							// update service
+							if service.Spec.ExternalName != dns {
+								log.Info().
+									Str("service", service.Name).
+									Str("old", service.Spec.ExternalName).
+									Str("new", dns).
+									Msg("Updating")
+
+								servicesClient := w.client.CoreV1().Services(ingress.Namespace)
+
+								retry.RetryOnConflict(retry.DefaultRetry, func() error {
+									result, getErr := servicesClient.Get(w.context, service.Name, v1.GetOptions{})
+									if getErr != nil {
+										log.Error().Err(getErr).Msg("Unable to retrieve service")
+										return nil
+									}
+									result.Spec.ExternalName = dns
+									_, updateErr := servicesClient.Update(w.context, result, v1.UpdateOptions{})
+									if updateErr == nil {
+										log.Info().
+											Str("service", service.Name).
+											Str("old", service.Spec.ExternalName).
+											Str("new", dns).
+											Msg("Update completed")
+									}
+									return updateErr
+								})
+							}
+						}
+					}
+				}
+
 			}
+
 		}
 
-		w.onChange(payload)
 	}
 
 	debounced := debounce.New(time.Second)
@@ -160,15 +146,7 @@ func (w *Watcher) Run(ctx context.Context) error {
 	}
 
 	var wg sync.WaitGroup
-	/*
-		wg.Add(1)
-		go func() {
-			informer := factory.Core().V1().Secrets().Informer()
-			informer.AddEventHandler(handler)
-			informer.Run(ctx.Done())
-			wg.Done()
-		}()
-	*/
+
 	wg.Add(1)
 	go func() {
 		informer := factory.Networking().V1().Ingresses().Informer()
